c139d57eaee827c6c812ce1a7a136fe87003a269
AndreVale69 <sforzini31@gmail.com>
2024-06-03 13:11:42 +0200

    Compilation inside post; Modified Singularity compilation

--- job.sh
+++ job.sh
@@ -1,3 +1,15 @@
-unzip -o SE4HPC_project_part2.zip
+#!/bin/bash
 
-singularity run matrix-multiplication.sif > output.txt 2> errors.txt
+#SBATCH --job-name=matrixMultiplication
+#SBATCH --time=00:30
+#SBATCH --ntasks=2
+#SBATCH --nodes=1
+#SBATCH -o output.out # File to which STDOUT will be written
+#SBATCH -e errors.err # File to which STDERR will be written
+
+module load singularity
+
+export TMPDIR=$HOME/tmp
+mkdir -p $TMPDIR
+
+singularity exec --bind $TMPDIR:$TMPDIR  matrix-multiplication.sif bash -c "export OMPI_MCA_tmpdir_base=$TMPDIR && mpirun -np 2 /prj/main"
